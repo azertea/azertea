64f1502288d29c264057cd0b46c4ef76e46768fd
Benoît Sauvère <benoit.sauvere@teevity.com>
2013-12-04 23:44:05 +0100

    Created quick launch scripts for bash and batch

---
+++ run.bat
@@ -0,0 +1,2 @@
+REM Run the Laravel 4 server for developments pruposes
+php -S localhost:8080 server.php
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run the Laravel 4 server for developments pruposes
+php -S localhost:8080 server.php
