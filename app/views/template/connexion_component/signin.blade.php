00f35008591a14c5fa86f264f3399a0e67bd1937
superkebab <felgines.elliot@gmail.com>
2013-12-05 10:46:17 +0100

    Ajout vues connexion

---
+++ app/views/template/connexion_component/signin.blade.php
@@ -0,0 +1,44 @@
+
+    <div id="box_login">
+        <div class="container">
+            <div class="span12 box_wrapper">
+                <div class="span12 box">
+                    <div>
+                        <div class="head">
+                            <h4>Log in to your account</h4>
+                        </div>
+                        <div class="social">
+                            <a class="face_login" href="#">
+                                <span class="face_icon">
+                                    <img src="img/i_face.png" alt="fb" alt="" />
+                                </span>
+                                <span class="text">Sign in with Facebook</span>
+                            </a>
+                            <div class="division">
+                                <hr class="left" />
+                                <span>or</span>
+                                <hr class="right" />
+                            </div>
+                        </div>
+                        <div class="form">
+                            <form />
+                                <input type="text" placeholder="Email" />
+                                <input type="password" placeholder="Password" />
+                                <div class="remember">
+                                    <div class="left">
+                                        <input id="remember_me" type="checkbox" />
+                                        <label for="remember_me">Remember me</label>
+                                    </div>
+                                    <div class="right">
+                                        <a href="reset.html">Forgot password?</a>
+                                    </div>
+                                </div>
+                                <input type="submit" class="btn" value="Sign in" />
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <p class="already">Don't have an account? <a href="signup.html"> Sign up</a></p>
+            </div>
+        </div>
+    </div>
