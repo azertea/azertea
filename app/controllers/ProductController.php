f377a55c1d429fdb6d6564cc06a66caaa20009a4
superkebab <felgines.elliot@gmail.com>
2013-12-06 00:14:34 +0100

    productController

---
+++ app/controllers/ProductController.php
@@ -0,0 +1,36 @@
+<?php
+
+class ProductController extends BaseController {
+
+		public function getProductList()
+        {
+            $result = array(); // Requete toutes annonces pour id categorie donné
+            return Response::json($result);
+        }
+
+
+        public function getProductAdd()
+        {
+               
+        }
+        public function postProductAdd()
+        {
+               
+        }
+
+        public function getProductSend()
+        {
+               
+        }
+        public function postProductSend()
+        {
+               
+        }
+
+        public function getProduct()
+        {
+
+        }
+}
+        
+
