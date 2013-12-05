4a0dc102ec0e8d9de177e2c04db2c53b4c590dc3
Arget31 <njeremy12@gmail.com>
2013-12-05 23:13:54 +0100

    Ajout de SearchController
    qui transforme la recherche en mot cle

---
+++ app/controllers/SearchController.php
@@ -0,0 +1,10 @@
+﻿<?php
+
+class SearchController extends BaseController {
+	public function postSearch(){
+		$phraseRecherche= Input::get('search'); //recuperation de la phrase de recherche du front end
+		$motCle= explode (' ', $phraseRecherche);//traitement
+//appel des categorie de la base de donnee
+		View::make($motCle);//envoi des categorie de la base de donnee a Front end
+	}
+}
