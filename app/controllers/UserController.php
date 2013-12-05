058d6c70ce42434a36d2e6707d9d21b24c6b1838
ju4693 <ju4693@gmail.com>
2013-12-05 23:45:48 +0100

    User Suite III

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -2,64 +2,89 @@
 
 class UserController extends BaseController {
 
-	public function getLogin()
-	{
-		return View::make('user/login');
-	}
 
-	public function getInscritpion()
-	{
-		return View::make('user/inscription');
-	}
+        public function postTestMail()
+        {
+            // Récuperation des parametres
+            $email = Input::post('email');
+            $motDePasse = Input::post('motDePasse');
 
-	public function postLogin()
-	{
-		$pseudo = Input::get('pseudo');
-		$motDePasse = Input::get('motDePasse');
+            $count = User::where('email', '=', $email)->count();
+            if($count==0)
+            {
+                  $message = 'Inscription';  
+            }
+            else
+            {
+                    $message = 'Connexion';
+            }
+            $result = $message;
+            return Response::json($result);
+        }
 
-		$user = User::where('login', '=', $pseudo );
 
-		if($user->motDePasse == $motDePasse)
-		{
-			// TODO : Demaarer une session ???
-			// Notification("Connexion OK");
-		}
-		else
-		{
-			// Echec
-		}
+        public function getLogin()
+        {
+                return View::make('user/login');
+        }
 
-		// Retour a la page d'accueil
-		return View::make('/');
-	}
+        public function getInscritpion()
+        {
+                return View::make('user/inscription');
+        }
 
-	public function postInscription()
-	{
-		// Récuperation des parametres
-		$pseudo = Input::post('pseudo');
-		$email = Input::post('email');
-		$motDePasse = Input::post('motDePasse');
+        public function postLogin()
+        {
+                $pseudo = Input::get('pseudo');
+                $motDePasse = Input::get('motDePasse');
 
-		// TODO : Faire des vérifications ...
+                $result = array();
 
-		if($ok)
-		{
-			// Création de l'user en BDD
-			$user = new User();
-			$user->pseudo = $pseudo;
-			$user->email = $email;
-			$user->motDePasse = $motDePasse;
-			$user->save();
+                $user = User::where('login', '=', $pseudo );
 
-			// TODO : Mettre en place un systeme de notification
-			// Notification("Vous etes bien inscrit");
-		}
-		else
-		{
-			// Notification("Erreur lors de l'inscription");
-		}
-		
-		// Redirection sur la page d'accueil
-		return Redirect::to('/');
-	}
+                if($user->motDePasse == $motDePasse)
+                {
+                        // TODO : Demaarer une session ???$
+                        $result['message'] = "Connexion OK";
+                }
+                else
+                {
+                        $result['message'] = "Connexion fail";
+                }
+
+                // Retour a la page d'accueil
+                return Response::json($result);
+                
+        }
+
+        public function postInscription()
+        {
+                // Récuperation des parametres
+                $pseudo = Input::post('pseudo');
+                $email = Input::post('email');
+                $motDePasse = Input::post('motDePasse');
+
+                // TODO : Faire des vérifications ...
+
+                if($ok)
+                {
+                        // Création de l'user en BDD
+                        $user = new User();
+                        $user->pseudo = $pseudo;
+                        $user->email = $email;
+                        $user->motDePasse = $motDePasse;
+                        $user->save();
+
+                        // TODO : Mettre en place un systeme de notification
+                        $message = "Vous etes bien inscrit";
+                }
+                else
+                {
+
+                         $message = "Erreur lors de l'authentification";
+                }
+                
+                // Redirection sur la page d'accueil
+                return Redirect::to('/')->with('message', $message);
+        }
 }
