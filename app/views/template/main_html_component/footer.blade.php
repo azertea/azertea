e161a5ffc73bbf43f65ee94b32c68ca8d3684548
superkebab <felgines.elliot@gmail.com>
2013-12-05 10:30:21 +0100

    Fix vues template

---
+++ app/views/template/main_html_component/footer.blade.php
@@ -0,0 +1,35 @@
+<!-- starts footer -->
+    <div id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="span8">
+                    <h3>From the Blog</h3>
+                    <div class="blog_post">
+                        <div class="row">
+                            <div class="span2">
+                                <a href="blog.html">
+                                    <img src="img/pic_blog.png" alt="post image" class="img-circle" />
+                                </a>
+                            </div>
+                            <div class="span4">
+                                <a href="blog.html" class="title">
+                                    Lorem Ipsum is simply dummy 
+                                </a>
+                                <div class="intro">
+                                    There are many variations of passages of Lorem alteration in some form by injected look even slightly believable.
+                                </div>
+                                <div class="date">
+                                    Oct 22. 2012
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+               <!--CONTACT-->
+               @include ('template.main_html_component.contact') 
+            </div>
+            <hr />
+           <!--SOCIAL-->
+           @include ('template.main_html_component.social') 
+        </div>
+    </div>
