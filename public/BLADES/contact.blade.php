537c0b76fe1d5fe08c86365c9df3a88254c03776
superkebab <felgines.elliot@gmail.com>
2013-12-05 10:27:08 +0100

    fichiers blade template

---
+++ public/BLADES/contact.blade.php
@@ -0,0 +1,33 @@
+ <div class="span4 contact_us">
+                    <h3>Contact Us</h3>
+                    <form class="form-horizontal" />
+                        <div class="field">
+                            <div class="label_field">
+                                <label for="inputName">Name</label>
+                            </div>
+                            <input type="text" id="inputName" />
+                        </div>
+
+                        <div class="field">
+                            <div class="label_field">
+                                <label for="inputEmail">Email</label>
+                            </div>
+                            <input type="text" id="inputEmail" />
+                        </div>
+
+                        <div class="field">
+                            <div class="label_field">
+                                <label for="inputCity">City</label>
+                            </div>
+                            <input type="text" id="inputCity" />
+                        </div>
+
+                        <div class="field">
+                            <div class="label_field">
+                                <label for="inputComment">Comments</label>
+                            </div>
+                            <textarea name="comments"></textarea>
+                        </div>
+                        <button type="submit" class="btn">Send</button>
+                    </form>
+                </div>
